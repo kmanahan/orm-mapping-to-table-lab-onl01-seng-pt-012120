09cc1dd4d4d396b66277e874f1dcff16aa40b9ae
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-24 18:40:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,4 +19,15 @@ class Student
         SQL
     DB[:conn].execute(sql) 
   end 
+  
+  def self.drop_table 
+    sql = <<-SQL 
+      DROP TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end 
 end
