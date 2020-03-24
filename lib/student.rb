0d5421f4e777491d767f2ef19fbef57737bba5d0
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-24 18:38:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,13 @@ class Student
   end
   
   def self.create_table 
-    sql 
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end 
 end
