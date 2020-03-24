2bf14c94dd6a285be2ad6e10d39568ec9a71b74e
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-03-24 18:41:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
   
   def self.drop_table 
     sql = <<-SQL 
-      DROP TABLE students (
+      DROP TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         grade TEXT
